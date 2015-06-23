676c1758375f1ba6daf2849bf7ebc25427f703c5
TarCode <TarCode@github.com>
2015-06-23 09:14:30 +0200

    defined color variables, added main function

--- worm.py
+++ worm.py
@@ -18,4 +18,36 @@ assert WINDOWHEIGHT % CELLSIZE == 0, "Window width must be a multiple of cell si
 CELLWIDTH = int(WINDOWWIDTH/CELLSIZE)
 CELLHEIGHT = int(WINDOWHEIGHT/CELLSIZE)
 
+#	      R    G    B
+WHITE 	  = (230, 230, 230)
+BLACK 	  = ( 10,  10,  20)
+RED   	  = (230,   0,   0)
+GREEN 	  = (  0, 230,   0)
+DARKGREEN = (  0, 130,   0)
+DARKGRAY  = ( 30,  30,  30)
+
+BGCOLOR = BLACK
+
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
+
+HEAD = 0 #worms head
+
+# main
+def main():
+	global FPSCLOCK, DISPLAYSURF, BASICFONT
+
+	pygame.init()
+	FPSCLOCK = pygame.time.Clock()
+	DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+	BASICFONT = pygame.font.Font('freesansbold.ttf', 20)
+	pygame.display.set_caption('WORM')
+	
+	showStartScreen()
+	while True:
+		runGame()
+		showGameOverScreen()
+
 
